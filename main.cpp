78c3da8e38b7ba0daba5a1fb5644983e44457956
auralius <auralius.manurung@formulatrix.com>
2024-08-15 04:16:15 +0700

    Second commit ;-)

--- main.cpp
+++ main.cpp
@@ -5,11 +5,22 @@
 
 int main(int argc, char **argv)
 {
+    printf("This is with OpenMP\n");
     #pragma omp parallel for
     for (int i = 0; i < 10; i++) {
         sleep(1);
         printf("#%i has finished sleeping!\n", i);
+        fflush(stdout);
+    }
+
+
+    printf("\n\nThis is without OpenMP\n");
+    for (int i = 0; i < 10; i++) {
+        sleep(1);
+        printf("#%i has finished sleeping!\n", i);
+        fflush(stdout);
     }
 
     return 0;
+
 }
